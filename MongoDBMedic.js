d024025afcb5615c28f614691667a00c0413f343
Hanul <hanul@hanul.me>
2019-06-01 16:17:32 +0900

    개발 완료

--- MongoDBMedic.js
+++ MongoDBMedic.js
@@ -4,15 +4,109 @@ BOX('UMAIL');
 
 require('./UMAIL/NODE.js');
 
+let exec = require('child_process').exec;
+
+let run = (command, callback) => {
+	
+	exec(command, (error) => {
+		if (error !== TO_DELETE) {
+			SHOW_ERROR(command, error.toString());
+		} else if (callback !== undefined) {
+			callback();
+		}
+	});
+};
+
 let config = JSON.parse(READ_FILE({
 	path : 'config.json',
 	isSync : true
 }));
 
 CONNECT_TO_DB_SERVER({
-	host : config.mongoHost,
 	port : config.mongoPort,
 	name : config.testDBName,
 	username : config.testDBUsername,
 	password : config.testDBPassword
+});
+
+BOX('MongoDBMedic');
+
+let testDB = MongoDBMedic.DB('Test');
+
+UMAIL.CONNECT_TO_MAIL_SERVER({
+	host : config.mailHost,
+	port : config.mailPort,
+	isSecure : config.isMailSecure,
+	username : config.mailUsername,
+	password : config.mailPassword
+}, (_sendMail) => {
+	
+	let sendMail = (title, content) => {
+		_sendMail({
+			senderName : config.mailSenderName,
+			senderAddress : config.mailSenderAddress,
+			receiverAddress : config.mailReceiverAddress,
+			title : title,
+			content : content + '\n\nBy https://github.com/Hanul/MongoDBMedic'
+		});
+	};
+	
+	let isErrorOccured = false;
+	
+	// 1초에 한번씩 DB 체크
+	INTERVAL(1, RAR(() => {
+		
+		if (isErrorOccured !== true) {
+			
+			testDB.get({
+				
+				notExists : () => {
+					testDB.create({
+						ping : 'pong'
+					});
+				},
+				
+				// 오류 발생!!
+				error : () => {
+					isErrorOccured = true;
+					
+					SHOW_ERROR('MongoDB에 이상 현상이 발생했습니다.');
+					
+					// DB 복구 절차 수행
+					REPEAT(config.mongoDeamonCount, (i) => {
+						let index = i + 1;
+						
+						run('mongod --shardsvr --port 3000' + index + ' --fork --keyFile /srv/mongodb/mongodb-shard-keyfile --logpath /var/log/mongo_shard_db' + index + '.log --dbpath /data/shard_db' + index);
+					});
+					
+					DELAY(1, () => {
+						
+						// Config DB 복구 절차 수행
+						[
+							'mongod --configsvr --replSet csReplSet --port 40001 --fork --keyFile /srv/mongodb/mongodb-shard-keyfile --logpath /var/log/mongo_shard_config1.log --dbpath /data/shard_config1',
+							'mongod --configsvr --replSet csReplSet --port 40002 --fork --keyFile /srv/mongodb/mongodb-shard-keyfile --logpath /var/log/mongo_shard_config2.log --dbpath /data/shard_config2',
+							'mongod --configsvr --replSet csReplSet --port 40003 --fork --keyFile /srv/mongodb/mongodb-shard-keyfile --logpath /var/log/mongo_shard_config3.log --dbpath /data/shard_config3'
+						].forEach(run);
+					});
+					
+					DELAY(2, () => {
+						
+						// Mongos 복구 절차 수행
+						run('mongos --port 27018 --fork --keyFile /srv/mongodb/mongodb-shard-keyfile --logpath /var/log/mongo_shard_mongos.log --configdb csReplSet/localhost:40001,localhost:40002,localhost:40003 --bind_ip_all', () => {
+							
+							// 모든 forever 데몬 재시작
+							run('forever restartall', () => {
+								
+								console.log(CONSOLE_GREEN('복구를 완료하였습니다.'));
+								
+								sendMail(config.serverName + '의 MongoDB에 이상 현상이 발생해 복구하였습니다.', 'MongoDB에 이상 현상이 발생해 복구하였습니다.\n' + config.serverName + '을(를) 체크하시기 바랍니다.');
+							});
+						});
+					});
+				}
+			});
+		}
+	}));
+	
+	console.log(CONSOLE_GREEN('MongoDBMedic이 실행중입니다...'));
 });
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # MongoDBMedic
-[UPPERCASE의 분산 처리 환경](https://github.com/Hanul/UPPERCASE/blob/master/DOC/GUIDE/CLUSTERING.md)에서 몽고DB의 상태를 지속적으로 분석하여 오류가 발생하는 경우 재시작해주는 솔루션
+[UPPERCASE의 분산 처리 환경](https://github.com/Hanul/UPPERCASE/blob/master/DOC/GUIDE/CLUSTERING.md)에서 몽고DB의 상태를 지속적으로 분석하여 이상 현상이 발생하는 경우 재시작해주는 솔루션
+
+## 제한 사항
+`forever`로 실행한 Node.JS 프로세스가 MongoDB와 같은 환경에서 실행되어 있어야 합니다.
 
 ## 설치하기
 ```
@@ -41,8 +44,8 @@ forever start MongoDBMedic.js
 이제 MongoDB가 정상적으로 작동하지 않으면 MongoDB를 재시작하고, `forever restartall`을 수행한 뒤 다음과 같은 메일이 오게됩니다.
 
 ```
-제목: My Server의 MongoDB가 정상적으로 작동하지 않아 재시작하였습니다.
-내용: MongoDB가 정상적으로 작동하지 않아 재시작하였습니다.
+제목: My Server의 MongoDB에 이상 현상이 발생해 복구하였습니다.
+내용: MongoDB에 이상 현상이 발생해 복구하였습니다.
 My Server을(를) 체크하시기 바랍니다.
 ```
 
